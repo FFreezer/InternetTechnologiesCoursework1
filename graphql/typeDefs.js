f2e96530fee5cdd06cf77bba55c3a5bfc2ca6767
Fraser Fewster <fraserfewster@hotmail.co.uk>
2020-10-24 20:57:32 +0100

    typeDef changes
    not much to say. just a few tweaks here & there but nothing that really changes functionality

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -2,32 +2,32 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
     type Sale{
-        _id: ID
-        saleDate: String
-        storeLocation: String
-        couponUsed: Boolean
-        purchaseMethod: String
-        items: [Item]
-        customer: Customer
+        _id: ID!
+        saleDate: String!
+        storeLocation: String!
+        couponUsed: Boolean!
+        purchaseMethod: String!
+        items: [Item]!
+        customer: Customer!
     }
 
     type Item {
-        name: String
+        name: String!
         tags: [String]
-        price: Float
-        quantity: Int
+        price: Float!
+        quantity: Int!
     }
 
     type Customer {
         gender: String
         age: Int
-        email: String
-        satisfaction: Int
+        email: String!
+        satisfaction: Int!
     }
 
     input SaleInput {
-        storeLocation: String,
-        couponUsed: Boolean,
+        storeLocation: String
+        couponUsed: Boolean
         purchaseMethod: String
         items: [ItemInput]
         customer: CustomerInput
